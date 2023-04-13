b809027eba6bf7844f3504b9752ce4172630f314
사람 <ydwk07k@gmail.com>
2023-04-13 02:44:45 +0000

    fdg

---
+++ p1.c
@@ -0,0 +1,17 @@
+// 프로그램 : BMI 계산기
+//만든이 : 윤동욱
+//몸무게 (kg) / 키(m)
+
+#include <stdio.h>
+
+int main( )
+{
+
+	int num;
+	printf("정수를 입력 : ");
+	scanf("%d", &num);
+	
+	printf("%d\n", num);
+	
+	return 0;
+}
